be47e06efa522d15d0de96b7b676050d22a79daf
vitorpaixaosinqia <vitor.paixao@sinqia.com.br>
2020-01-17 17:44:26 -0300

    criando linha principal e duplicando, o que prepara para a criação do RepeatGrid - Fase 01 OK

--- main.js
+++ main.js
@@ -26,12 +26,6 @@ function create() {
             }
         </style>
         <form method="dialog" id="main">
-            <div class="row break">
-                <label class="row">
-                    <span>↕︎</span>
-                    <input type="text" uxp-quiet="true" id="txtInput" value="" placeholder="List" />
-                </label>
-            </div>
             <footer><button id="load" type="submit" uxp-variant="cta">Load Data</button></footer>
             <footer><button id="create" type="submit" uxp-variant="cta">Create</button></footer>
         </form>
@@ -40,49 +34,73 @@ function create() {
 
     function createTable() {
         const { editDocument } = require("application");
-        const textInput = document.querySelector("#txtInput").value;
-
-        editDocument({ editLabel: "Increase rectangle size" }, function (selection) {
-            console.log(selection.items.length + " items are selected");
+        // const textInput = document.querySelector("#txtInput").value;
         
-            // console.log('data::: ', data);
+        // console.log('data>>>>>>>>>>>>>>>>>>>>>>\n', data[0]);
+
+        editDocument({ editLabel: "Increase rectangle size" },  async function (selection) {
+            // console.log(selection.items.length + " items are selected");
+            
+            const data = await insertTextFromFileHandler();
+            // console.log('data<><><><><><><><><><><>\n', data[0]);
         
             const selectedRectangle = selection.items[0];
-            // const widthRectangle = selectedRectangle.width;
+            const widthRectangle = selectedRectangle.width;
+            const heightRectangle = selectedRectangle.height;
+            
             commands.group();
             const group = selection.items[0];
         
             // Create original Text
+
+            // console.log('data[0][0]>>>>>>>:::', data[0][0]);
             
             const textInputed = new Text();
             textInputed.fill = new Color("#000000");
             textInputed.fontSize = 11;
-            textInputed.width = 100;
-            textInputed.text = textInput;
+            textInputed.text = data[0][0];
+
             group.addChildAfter(textInputed, selectedRectangle);
             
             selection.items = [textInputed, selectedRectangle];
             commands.alignRight();
             commands.alignLeft();
             commands.alignVerticalCenter();
-            textInputed.moveInParentCoordinates(20,0);
+            textInputed.moveInParentCoordinates(16,0);
             
-            const texts = [textInput, textInput, textInput, textInput];
-            // selectedRectangle.width = widthRectangle * texts.length;
-            console.log('texts', texts);
+            const texts = data[0];
+            // console.log('texts:::' + texts)
+            selectedRectangle.width = widthRectangle * texts.length;
+            // console.log('texts', texts);
             
             texts.forEach((e, i) => {
-                console.log('e::: ', e);
-                console.log('i::: ', i);
+                if (e === "") {
+                    e = "___"
+                }
+                // console.log('e::: ', e);
+                // console.log('i::: ', i);
                 selection.items = textInputed;
                 commands.duplicate();
                 selection.items[0].text = e;
                 selection.items[0].name = e;
                 group.addChildAfter(selection.items[0], selectedRectangle);
                 selection.items = [selection.items[0], selectedRectangle];
-                console.log('mode -> ', Number(100*(i+1)));
-                selection.items[0].moveInParentCoordinates(Number(100*(i+1)),0);
+                if (i == 0){
+                    selection.items[0].moveInParentCoordinates(16,0);
+                } else {
+                    selection.items[0].moveInParentCoordinates(Number( widthRectangle * (i+1) ),0);
+                }
             });
+            
+            textInputed.removeFromParent();
+
+            selection.items = group;
+            commands.duplicate();
+            selection.items[0].moveInParentCoordinates(0,heightRectangle-1);
+
+            // const newGroup = selection.items[0];
+            // commands.duplicate();
+            // newGroup.moveInParentCoordinates(heightRectangle -1,0);
             
         })
     }
@@ -100,8 +118,9 @@ function show(event) {
 }
 
 function update() {
-    console.log(selection.items.length + " items are selected");
-    console.log('update');
+    // console.log(selection.items.length + " items are selected");
+    // console.log('update');
+    console.log(selection.items);
     const { Rectangle } = require("scenegraph");
     let form = document.querySelector("form");
     let warning = document.querySelector("#warning");
@@ -115,7 +134,7 @@ function update() {
 }
 
 async function insertTextFromFileHandler() {
-    console.log('insertTextFromFileHandler');
+    // console.log('>>> insertTextFromFileHandler');
     const aFile = await fs.getFileForOpening({ types: ["csv"] });
     if (!aFile)
         return;
@@ -125,23 +144,25 @@ async function insertTextFromFileHandler() {
     if (!contents)
         return;
     
-    console.log('contents:', contents);
-    console.log('\n');
+    // console.log('contents:', contents);
+    // console.log('\n');
 
     const data = {};
     const rows = contents.split('\n');
     
-    console.log('rows', rows);
-    console.log('\n');
+    // console.log('rows', rows);
+    // console.log('\n');
     
     rows.forEach((e, i) => {
-        console.log('i::::::::::::::::', i);
+        // console.log('i::::::::::::::::', i);
         const row = e.split(',');
-        console.log('row: ', row);
+        // console.log('row: ', row);
         
         data[i] = row;
-        console.log('data[i] : ', data[i]);
+        // console.log('data[i] : ', data[i]);
     });
+    // console.log('data::::::::::::::::::::::::::::::::::::::::::::::: ', data , '\n');
+    return data;
     
 }
 
