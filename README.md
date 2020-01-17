d2c90947235247ea30e96fd43155ebe5de024052
Vitor Paixão <vitorpaixao@users.noreply.github.com>
2020-01-17 17:46:11 -0300

    Develop (#1)
    * update with tutoriais
    
    * loading files and creating items
    
    * criando linha principal e duplicando, o que prepara para a criação do RepeatGrid - Fase 01 OK

--- README.md
+++ README.md
@@ -3,6 +3,27 @@
 This sample plugin is a persistent panel. Users can increase the width and height of the selected rectangle by inputing pixel values in the panel.
 [Read the step-by-step guide for this sample](https://adobexdplatform.com/plugin-docs/tutorials/quick-start-panel/).
 
+## Tutoriais
+https://github.com/AdobeXD/plugin-docs
+https://github.com/AdobeXD/plugin-docs/blob/master/tutorials/how-to-create-paths/index.md
+https://github.com/AdobeXD/plugin-docs/blob/master/tutorials/how-to-style-text/index.md
+
+https://adobexdplatform.com/plugin-docs/reference/scenegraph.html
+https://adobexdplatform.com/plugin-docs/reference/selection.html
+
+## Using
+https://github.com/AdobeXD/xdpm
+https://github.com/AdobeXD
+
+# OBS
+
+- In VSCode "xdpm" not running in PowerShell
+
+
 ## Usage
 
-1. Run "Plugins > Enlarge a Rectangle"
+npm install -g @adobe/xdpm
+
+Run "xdpm watch"
+
+1. Run "Plugins > "
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { selection, Text, Color } = require("scenegraph")
+const { selection, Text, Color } = require("scenegraph");
+const fs = require("uxp").storage.localFileSystem;
+let commands = require("commands");
 let panel;
 
 function create() {
@@ -24,48 +26,89 @@ function create() {
             }
         </style>
         <form method="dialog" id="main">
-            <div class="row break">
-                <label class="row">
-                    <span>↕︎</span>
-                    <input type="text" uxp-quiet="true" id="txtInput" value="" placeholder="List" />
-                </label>
-                <label class="row">
-                    <span>↕︎</span>
-                    <input type="number" uxp-quiet="true" id="txtV" value="10" placeholder="Height" />
-                </label>
-                <label class="row">
-                    <span>↔︎</span>
-                    <input type="number" uxp-quiet="true" id="txtH" value="10" placeholder="Width" />
-                </label>
-            </div>
-            <footer><button id="ok" type="submit" uxp-variant="cta">Apply</button></footer>
+            <footer><button id="load" type="submit" uxp-variant="cta">Load Data</button></footer>
+            <footer><button id="create" type="submit" uxp-variant="cta">Create</button></footer>
         </form>
         <p id="warning">This plugin requires you to select a rectangle in the document. Please select a rectangle.</p>
         `
-    function increaseRectangleSize() {
+
+    function createTable() {
         const { editDocument } = require("application");
-        const height = Number(document.querySelector("#txtV").value);
-        const width = Number(document.querySelector("#txtH").value);
-        const textInput = document.querySelector("#txtInput").value;
+        // const textInput = document.querySelector("#txtInput").value;
+        
+        // console.log('data>>>>>>>>>>>>>>>>>>>>>>\n', data[0]);
 
-        editDocument({ editLabel: "Increase rectangle size" }, function (selection) {
+        editDocument({ editLabel: "Increase rectangle size" },  async function (selection) {
+            // console.log(selection.items.length + " items are selected");
+            
+            const data = await insertTextFromFileHandler();
+            // console.log('data<><><><><><><><><><><>\n', data[0]);
+        
             const selectedRectangle = selection.items[0];
-            selectedRectangle.width += width
-            selectedRectangle.height += height
+            const widthRectangle = selectedRectangle.width;
+            const heightRectangle = selectedRectangle.height;
+            
+            commands.group();
+            const group = selection.items[0];
+        
+            // Create original Text
 
-            const node = new Text();
-            node.fill = new Color("#ff0000");
-            node.fontSize = 24;
-            node.text = textInput;
+            // console.log('data[0][0]>>>>>>>:::', data[0][0]);
+            
+            const textInputed = new Text();
+            textInputed.fill = new Color("#000000");
+            textInputed.fontSize = 11;
+            textInputed.text = data[0][0];
 
-            selection.insertionParent.addChild(node);
-            node.moveInParentCoordinates(20,50);
+            group.addChildAfter(textInputed, selectedRectangle);
+            
+            selection.items = [textInputed, selectedRectangle];
+            commands.alignRight();
+            commands.alignLeft();
+            commands.alignVerticalCenter();
+            textInputed.moveInParentCoordinates(16,0);
+            
+            const texts = data[0];
+            // console.log('texts:::' + texts)
+            selectedRectangle.width = widthRectangle * texts.length;
+            // console.log('texts', texts);
+            
+            texts.forEach((e, i) => {
+                if (e === "") {
+                    e = "___"
+                }
+                // console.log('e::: ', e);
+                // console.log('i::: ', i);
+                selection.items = textInputed;
+                commands.duplicate();
+                selection.items[0].text = e;
+                selection.items[0].name = e;
+                group.addChildAfter(selection.items[0], selectedRectangle);
+                selection.items = [selection.items[0], selectedRectangle];
+                if (i == 0){
+                    selection.items[0].moveInParentCoordinates(16,0);
+                } else {
+                    selection.items[0].moveInParentCoordinates(Number( widthRectangle * (i+1) ),0);
+                }
+            });
+            
+            textInputed.removeFromParent();
+
+            selection.items = group;
+            commands.duplicate();
+            selection.items[0].moveInParentCoordinates(0,heightRectangle-1);
+
+            // const newGroup = selection.items[0];
+            // commands.duplicate();
+            // newGroup.moveInParentCoordinates(heightRectangle -1,0);
+            
         })
     }
 
     panel = document.createElement("div");
     panel.innerHTML = HTML;
-    panel.querySelector("form").addEventListener("submit", increaseRectangleSize);
+    panel.querySelector("#load").addEventListener('click', insertTextFromFileHandler);
+    panel.querySelector("#create").addEventListener('click', createTable);
 
     return panel;
 }
@@ -75,6 +118,9 @@ function show(event) {
 }
 
 function update() {
+    // console.log(selection.items.length + " items are selected");
+    // console.log('update');
+    console.log(selection.items);
     const { Rectangle } = require("scenegraph");
     let form = document.querySelector("form");
     let warning = document.querySelector("#warning");
@@ -87,6 +133,38 @@ function update() {
     }
 }
 
+async function insertTextFromFileHandler() {
+    // console.log('>>> insertTextFromFileHandler');
+    const aFile = await fs.getFileForOpening({ types: ["csv"] });
+    if (!aFile)
+        return;
+
+    const contents = await aFile.read();
+
+    if (!contents)
+        return;
+    
+    // console.log('contents:', contents);
+    // console.log('\n');
+
+    const data = {};
+    const rows = contents.split('\n');
+    
+    // console.log('rows', rows);
+    // console.log('\n');
+    
+    rows.forEach((e, i) => {
+        // console.log('i::::::::::::::::', i);
+        const row = e.split(',');
+        // console.log('row: ', row);
+        
+        data[i] = row;
+        // console.log('data[i] : ', data[i]);
+    });
+    // console.log('data::::::::::::::::::::::::::::::::::::::::::::::: ', data , '\n');
+    return data;
+    
+}
 
 module.exports = {
     panels: {
@@ -94,5 +172,5 @@ module.exports = {
             show,
             update
         }
-    }
+    },
 };
